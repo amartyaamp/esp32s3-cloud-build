0ddfb0bedb441704a08d15f7ea2c2e11fa7a71e3
Amartya Chaudhuri <amartyaamp@users.noreply.github.com>
2025-07-28 00:54:35 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
-#include <Arduino.h>
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
+
 void setup() {
-    Serial.begin(115200);
-    Serial.println("Hello from Cloud Build!");
+  tft.init();
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setCursor(10, 20);
+  tft.println("Hello ESP32-S3 LCD 1.47!");
 }
+
 void loop() {
-    Serial.println("Running...");
-    delay(1000);
+  tft.setCursor(10, 40);
+  tft.println("It works!");
+  delay(1000);
 }
