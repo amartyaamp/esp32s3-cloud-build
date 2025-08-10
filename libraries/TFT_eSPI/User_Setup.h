0cfb130b665d6f4fb24bb4a5ef82855fe6cf704c
Amartya Chaudhuri <amartyaamp@users.noreply.github.com>
2025-08-10 14:33:25 +0530

    Add initial support for ST7789 display and various font configurations

--- libraries/TFT_eSPI/User_Setup.h
+++ libraries/TFT_eSPI/User_Setup.h
@@ -1,20 +1,21 @@
 #define ST7789_DRIVER
+#define TFT_WIDTH 172
+#define TFT_HEIGHT 320
 
-#define TFT_WIDTH  240
-#define TFT_HEIGHT 240
-
-// Waveshare ESP32-S3 LCD 1.47" pins
 #define TFT_MISO -1
 #define TFT_MOSI 35
 #define TFT_SCLK 36
 #define TFT_CS   34
 #define TFT_DC   37
 #define TFT_RST  38
-#define TFT_BL   45
-
-#define TFT_BACKLIGHT_ON HIGH
+#define TFT_BL   33
 
 #define LOAD_GLCD
 #define LOAD_FONT2
 #define LOAD_FONT4
-#define SMOOTH_FONT
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+
+#define SPI_FREQUENCY  40000000
