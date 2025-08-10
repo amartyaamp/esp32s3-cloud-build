12ba606ecdf6a8b86a1007a3d9aed56f86d0da34
Amartya Chaudhuri <amartyaamp@users.noreply.github.com>
2025-08-10 13:46:05 +0530

    Update TFT_eSPI User_Setup.h for ST7789 display dimensions and pin configuration

--- TFT_eSPI/User_Setup.h
+++ TFT_eSPI/User_Setup.h
@@ -1,14 +1,13 @@
 #define ST7789_DRIVER
 
-#define TFT_WIDTH  240
-#define TFT_HEIGHT 240
+#define TFT_WIDTH  172
+#define TFT_HEIGHT 320
 
-#define TFT_MISO -1
-#define TFT_MOSI 13
-#define TFT_SCLK 14
-#define TFT_CS   15
-#define TFT_DC    2
-#define TFT_RST   4
-#define TFT_BLK   21
+#define TFT_MOSI 45
+#define TFT_SCLK 40
+#define TFT_CS   42
+#define TFT_DC   41
+#define TFT_RST  39
+#define TFT_BL   48
 
 #define SPI_FREQUENCY  40000000
